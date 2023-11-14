705b55c30e5ab1f14961af7e2400fec0bbfcd723
Aysgl <aysegul.avcu@hotmail.com>
2023-11-14 05:23:54 +0300

    style and top button

--- src/component/Top/index.js
+++ src/component/Top/index.js
@@ -1,32 +1,29 @@
-import React from 'react'
-import $ from "jquery"
+import React, { useEffect } from 'react';
 
 const Top = () => {
-    // back to top
-    var offset = 300,
-        offset_opacity = 1200,
-        scroll_top_duration = 700,
-        $back_to_top = $('.cd-top');
+    useEffect(() => {
+        const handleScroll = () => {
+            const backToTop = document.querySelector('.cd-top');
+            backToTop.classList.toggle('cd-is-visible', window.scrollY > 300);
+            backToTop.classList.toggle('cd-fade-out', window.scrollY > 1200);
+        };
 
-    //hide or show the "back to top" link
-    $(window).scroll(function () {
-        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-        if ($(this).scrollTop() > offset_opacity) {
-            $back_to_top.addClass('cd-fade-out');
-        }
-    });
+        window.addEventListener('scroll', handleScroll);
 
-    //smooth scroll to top
-    $back_to_top.on('click', function (event) {
-        event.preventDefault();
-        $('body,html').animate({
-            scrollTop: 0,
-        }, scroll_top_duration);
-    });
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
 
     return (
-        <a href="#0" className="cd-top"><i className="ion-android-arrow-up"></i></a>
-    )
-}
+        <a href="#" className="cd-top" onClick={scrollToTop}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" /></svg>
+        </a>
+    );
+};
 
-export default Top
+export default Top;
